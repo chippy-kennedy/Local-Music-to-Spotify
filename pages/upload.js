8ee83fd36474c25421de09b72819b7318fb7b7c4
Chip Kennedy <chip@docollectively.com>
2021-03-20 19:35:06 -0400

    Add step-2 page with upload UI + add upload api route (WIP)

--- pages/upload.js
+++ pages/upload.js
@@ -2,9 +2,67 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React, { useState } from 'react';
 import Link from 'next/link'
+import Uppy from "@uppy/core"
+import { DragDrop } from "@uppy/react"
+import ThumbnailGenerator from "@uppy/thumbnail-generator"
+import XHRUpload from "@uppy/xhr-upload"
+import { StatusBar } from '@uppy/react'
+import '@uppy/core/dist/style.css'
+import '@uppy/status-bar/dist/style.css'
 
-export default function Home() {
+const uppy = new Uppy({
+  meta: { type: "music-upload" },
+  restrictions: {
+    maxNumberOfFiles: 3,
+    maxFileSize: 1048576 * 16,
+    allowedFileTypes: [".mp3", ".mp4"],
+  },
+  autoProceed: true,
+})
+
+uppy.use(XHRUpload, {
+  endpoint: "/api/upload",
+  fieldName: "music-upload",
+  formData: true,
+})
+
+uppy.use(ThumbnailGenerator, {
+  thumbnailWidth: 200,
+  waitForThumbnailsBeforeUpload: false,
+})
+
+uppy.on("thumbnail:generated", (file, preview) => {
+  console.log(file.name, preview)
+})
+
+uppy.on("complete", result => {
+  const url = result.successful[0].uploadURL
+  console.log("successful upload", result)
+})
+
+uppy.on("error", error => {
+  console.error(error.stack)
+})
+
+uppy.on("restriction-failed", (file, error) => {
+  const err = error.stack.includes("exceeds maximum allowed size of 16 MB")
+    ? "A fájl mérete nagyobb mint 4MB"
+    : error
+
+  alert(
+    "Feltöltési hiba: " +
+      err +
+      "\n" +
+      file.name +
+      " Mérete : " +
+      Math.round(file.size / 1024 / 1024) +
+      "MB"
+  )
+})
+
+export default function Upload() {
 	const [state, setState] = useState('new');
+
 
   return (
 		<div className="flex flex-col items-center justify-center min-h-screen pt-2 bg-green">
@@ -16,28 +74,6 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center flex-1 px-0 text-center space-y-5 w-full">
 				<div className="section w-full mx-0 mt-24 pb-6 bg-green">
-					{state == 'new' ?
-						<div className="container mx-auto mt-12 w-full">
-							<Image
-								src="/laptop-upload.svg"
-								alt="Laptop with upload icon"
-								width="175"
-								height="175"
-							/>
-							<h1 className="text-5xl font-bold mt-6">
-								Save Local Music to Spotify
-							</h1>
-							<div className="my-6 max-w-screen-md mx-auto">
-								<p>Save your local music library to Spotify without needing to download your library to every device</p>
-							</div>
-
-							<div className="my-8">
-								<button className="btn btn-white" onClick={() => setState('step-1')}>
-									Get Started
-								</button>
-							</div>
-						</div>
-						:
 						<div className="container mx-auto mt-12 w-full">
 							<Image
 								src="/laptop-upload.svg"
@@ -54,24 +90,40 @@ export default function Home() {
 
 							<div className="">
 								<h3 className="text-3xl font-bold mt-12">
-									Step 1: Log in with Spotify
+									Step 2: Upload Your Local Music
 								</h3>
 							</div>
 							<div className="my-6 max-w-screen-md mx-auto">
 								<p>
-									You'll be redirected to Spotify and then sent back here
+									Upload your music below. Click and drag or click the button to upload.
 								</p>
 							</div>
 
 							<div className="my-8 mx-auto">
-								<Link href="/api/authenticate" className="btn-orange mx-auto">
-									<div className="btn btn-orange">
-										Log in with Spotify
-									</div>
-								</Link>
+								<div className="btn btn-orange">
+									<DragDrop
+										uppy={uppy}
+										locale={{
+											strings: {
+												// Text to show on the droppable area.
+												// `%{browse}` is replaced with a link that opens the system file selection dialog.
+												dropHereOr: "Upload Music",
+												// Used as the label for the link that opens the system file selection dialog.
+												browse: "Browse for Local Music",
+											},
+										}}
+									/>
+								</div>
+								<StatusBar
+									uppy={uppy}
+									hideUploadButton
+									hideAfterFinish={false}
+									showProgressDetails
+								/>
+
+
 							</div>
 						</div>
-					}
 				</div>
 
 				<div className="section w-full py-10 my-12 bg-white">
